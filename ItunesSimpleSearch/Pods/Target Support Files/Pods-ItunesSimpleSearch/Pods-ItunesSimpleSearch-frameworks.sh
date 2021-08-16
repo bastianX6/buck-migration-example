b0cd6856d0be538ba1ef2a309a7e41362874d340
Bastian Veliz <bastianx6@gmail.com>
2021-08-16 15:14:48 -0400

    [buck] try to compile app

--- ItunesSimpleSearch/Pods/Target Support Files/Pods-ItunesSimpleSearch/Pods-ItunesSimpleSearch-frameworks.sh
+++ ItunesSimpleSearch/Pods/Target Support Files/Pods-ItunesSimpleSearch/Pods-ItunesSimpleSearch-frameworks.sh
@@ -175,12 +175,12 @@ code_sign_if_enabled() {
 }
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SDWebImageSwiftUI/SDWebImageSwiftUI.framework"
+  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/SDWebImage/SDWebImage.framework"
+  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/SDWebImageSwiftUI/SDWebImageSwiftUI.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/SDWebImage/SDWebImage.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/SDWebImageSwiftUI/SDWebImageSwiftUI.framework"
+  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/SDWebImage/SDWebImage.framework"
+  install_framework "${PODS_XCFRAMEWORKS_BUILD_DIR}/SDWebImageSwiftUI/SDWebImageSwiftUI.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
